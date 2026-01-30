9633ab95703cb0cbf2665ea2cce773c0dfe16469
diartejm <diarte.jm@slalom.com>
2026-01-30 17:32:38 -0500

    Fix failed tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -24,7 +24,7 @@ func TestGetAPIKey(t *testing.T) {
 			headers: http.Header{
 				"Authorization": {""},
 			},
-			expectedKey:   "test",
+			expectedKey:   "",
 			expectedError: ErrNoAuthHeaderIncluded,
 		},
 		{
